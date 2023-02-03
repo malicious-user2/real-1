d8d3b62946a079fc367297e57457b2e3e626ce8c
cantest-nospam <cantest-nospam>
2023-02-03 16:07:24 -0500

    wip

--- src/YouRatta.Common/Common/GitHub/GitHubEnvironment.cs
+++ src/YouRatta.Common/Common/GitHub/GitHubEnvironment.cs
@@ -6,8 +6,16 @@ public class GitHubEnvironment
 {
     public GitHubEnvironment()
     {
-        this.REPOSITORY = string.Empty;
+        this.Repository = string.Empty;
     }
 
-    public string REPOSITORY { get; set; }
+    public string Repository { get; set; }
+
+    public string Repository_Owner { get; set; }
+
+    public int Run_Attempt { get; set; }
+
+    public int Server_URL { get; set; }
+
+    public string Triggering_Actor { get; set; }
 }

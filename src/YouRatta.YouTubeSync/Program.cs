bb99c4dc7ee6bf0c582786eebb803a5a5a84bc37
cantest-nospam <nospam@aswglobal.com>
2023-02-26 13:46:21 -0500

    wip

--- src/YouRatta.YouTubeSync/Program.cs
+++ src/YouRatta.YouTubeSync/Program.cs
@@ -56,61 +56,39 @@ using (YouTubeSyncCommunicationClient client = new YouTubeSyncCommunicationClien
                 {
                     ApiKey = actionInt.AppApiKey,
                     ApplicationName = YouTubeConstants.RequestApplicationName,
-                    HttpClientInitializer = userCred,
-                    BaseUri = "http://localhost"
+                    HttpClientInitializer = userCred
                 }))
     {
         List<ResourceId> ignoreResources = YouTubePlaylistHelper.GetPlaylistVideos(config.YouTube.ExcludePlaylists, ytService);
-
-        SearchResource.ListRequest searchRequest = new SearchResource.ListRequest(ytService, new string[] { YouTubeConstants.RequestSnippetPart });
-        if (string.IsNullOrEmpty(config.YouTube.ChannelId)) return;
-        searchRequest.ChannelId = config.YouTube.ChannelId;
-        searchRequest.MaxResults = 50;
-        bool requestNextPage = true;
-        while (requestNextPage)
+        GitHubClient ghClient = new GitHubClient(GitHubConstants.ProductHeader)
         {
-            GitHubClient ghClient = new GitHubClient(GitHubConstants.ProductHeader)
+            Credentials = new Credentials(actionEnvironment.ApiToken, AuthenticationType.Bearer)
+        };
+        ghClient.SetRequestTimeout(GitHubConstants.RequestTimeout);
+        string[] repository = actionEnvironment.EnvGitHubRepository.Split("/");
+        List<Video> videoList = YouTubeVideoHelper.GetChannelVideos(config.YouTube.ChannelId, ignoreResources, ytService);
+        foreach (Video video in videoList)
+        {
+            string errataBulletinPath = $"{ErrataBulletinConstants.ErrataRootDirectory}" +
+                $"{video.Id}.md";
+            if (!Path.Exists(Path.Combine(Directory.GetCurrentDirectory(), GitHubConstants.ErrataCheckoutPath, errataBulletinPath)))
             {
-                Credentials = new Credentials(actionEnvironment.ApiToken, AuthenticationType.Bearer)
-            };
-            ghClient.SetRequestTimeout(GitHubConstants.RequestTimeout);
-            string[] repository = actionEnvironment.EnvGitHubRepository.Split("/");
-            SearchListResponse searchResponse = searchRequest.Execute();
-            foreach (SearchResult searchResult in searchResponse.Items)
-            {
-                if (searchResult.Id.Kind != YouTubeConstants.VideoKind) continue;
-                string errataBulletinPath = $"{ErrataBulletinConstants.ErrataRootDirectory}" +
-                    $"{searchResult.Id.VideoId}.md";
-                if (!Path.Exists(Path.Combine(Directory.GetCurrentDirectory(), GitHubConstants.ErrataCheckoutPath, errataBulletinPath)))
-                {
-                    VideosResource.ListRequest videoRequest = new VideosResource.ListRequest(ytService, new string[] { YouTubeConstants.RequestContentDetailsPart });
-                    videoRequest.Id = searchResult.Id.VideoId;
-                    videoRequest.MaxResults = 1;
-                    VideoListResponse videoResponse = videoRequest.Execute();
-                    Video videoDetails = videoResponse.Items.First();
-                    ErrataBulletinBuilder bulletinBuilder = new ErrataBulletinBuilder(config.ErrataBulletin);
-                    string ytVideoTitle = WebUtility.HtmlDecode(searchResult.Snippet.Title);
-                    string videoTitle = string.IsNullOrEmpty(ytVideoTitle) ? "Unknown Video" : ytVideoTitle;
-                    bulletinBuilder.SnippetTitle = videoTitle;
-                    bulletinBuilder.ContentDuration = XmlConvert.ToTimeSpan(videoDetails.ContentDetails.Duration);
-                    string errataBulletin = bulletinBuilder.Build();
+                ErrataBulletinBuilder bulletinBuilder = new ErrataBulletinBuilder(config.ErrataBulletin);
+                string ytVideoTitle = WebUtility.HtmlDecode(video.Snippet.Title);
+                string videoTitle = string.IsNullOrEmpty(ytVideoTitle) ? "Unknown Video" : ytVideoTitle;
+                bulletinBuilder.SnippetTitle = videoTitle;
+                bulletinBuilder.ContentDuration = XmlConvert.ToTimeSpan(video.ContentDetails.Duration);
+                string errataBulletin = bulletinBuilder.Build();
 
-
-                    CreateFileRequest createFile = new CreateFileRequest(videoTitle, errataBulletin, GitHubConstants.ErrataBranch);
-                    ghClient.Repository.Content.CreateFile(repository[0], repository[1], errataBulletinPath, createFile).Wait();
-                }
+                CreateFileRequest createFile = new CreateFileRequest(videoTitle, errataBulletin, GitHubConstants.ErrataBranch);
+                ghClient.Repository.Content.CreateFile(repository[0], repository[1], errataBulletinPath, createFile).Wait();
 
             }
-            requestNextPage = !string.IsNullOrEmpty(searchResponse.NextPageToken);
-            searchRequest.PageToken = searchResponse.NextPageToken;
         }
-
 
         foreach (string dir in Directory.GetFiles(Path.Combine(Directory.GetCurrentDirectory(), "errata")))
         {
             Console.WriteLine(dir);
         }
-
-        Console.WriteLine(XmlConvert.ToTimeSpan("PT4M13S").ToString());
     }
 }
---
+++ src/YouRatta.YouTubeSync/YouTube/YouTubeVideoHelper.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Xml;
+using Google.Apis.YouTube.v3;
+using Google.Apis.YouTube.v3.Data;
+using YouRatta.Common.YouTube;
+using YouRatta.YouTubeSync.ErrataBulletin;
+
+namespace YouRatta.YouTubeSync.YouTube;
+
+internal static class YouTubeVideoHelper
+{
+
+    public static List<Video> GetChannelVideos(string channelId, List<ResourceId> excludeVideos, YouTubeService service)
+    {
+        List<Video> channelVideos = new List<Video>();
+        SearchResource.ListRequest searchRequest = new SearchResource.ListRequest(service, new string[] { YouTubeConstants.RequestSnippetPart });
+        if (string.IsNullOrEmpty(channelId)) return channelVideos;
+        searchRequest.ChannelId = channelId;
+        searchRequest.MaxResults = 50;
+        bool requestNextPage = true;
+        while (requestNextPage)
+        {
+            SearchListResponse searchResponse = searchRequest.Execute();
+            foreach (SearchResult searchResult in searchResponse.Items)
+            {
+                VideosResource.ListRequest videoRequest = new VideosResource.ListRequest(service, new string[] { YouTubeConstants.RequestContentDetailsPart });
+                videoRequest.Id = searchResult.Id.VideoId;
+                videoRequest.MaxResults = 1;
+                VideoListResponse videoResponse = videoRequest.Execute();
+                Video videoDetails = videoResponse.Items.First();
+                if (excludeVideos != null && excludeVideos.Find(resourceId => resourceId.VideoId == videoDetails.Id) != null) continue;
+                channelVideos.Add(videoDetails);
+            }
+            requestNextPage = !string.IsNullOrEmpty(searchResponse.NextPageToken);
+            searchRequest.PageToken = searchResponse.NextPageToken;
+        }
+        return channelVideos;
+    }
+}

ec814b67f33d64341125eb7825425490eafc1304
cantest-nospam <103596984+cantest-nospam@users.noreply.github.com>
2023-04-05 11:23:57 -0400

    wip

--- src/YouRata.InitialSetup/Program.cs
+++ src/YouRata.InitialSetup/Program.cs
@@ -18,7 +18,7 @@ using YouRata.InitialSetup.Workflow;
 using static YouRata.Common.Proto.MilestoneActionIntelligence.Types;
 
 /// ---------------------------------------------------------------------------------------------
-/// The Initial Setup milestone verifies the existence of action secrets and variables.
+/// The InitialSetup milestone verifies the existence of action secrets and variables.
 /// Control is started from the Run YouRata action in the event TOKEN_RESPONSE is not a populated
 /// environment variable. Step feedback is generated in the GITHUB_STEP_SUMMARY workflow
 /// command and provides further instructions after each run. Finally the program allows the
@@ -27,17 +27,23 @@ using static YouRata.Common.Proto.MilestoneActionIntelligence.Types;
 
 using (InitialSetupCommunicationClient client = new InitialSetupCommunicationClient())
 {
+    // Notify ConflictMonitor that the InitialSetup milestone is starting
     if (!client.Activate(out InitialSetupActionIntelligence milestoneInt)) return;
+    // Stop if InitialSetup is disabled
     if (client.GetYouRataConfiguration().ActionCutOuts.DisableInitialSetupMilestone) return;
+    // Fill runtime variables
     MilestoneVariablesHelper.CreateRuntimeVariables(client, out ActionIntelligence actionInt, out YouRataConfiguration config,
         out GitHubActionEnvironment actionEnvironment);
+    // Get workflow variables
     InitialSetupWorkflow workflow = new InitialSetupWorkflow();
     try
     {
         bool canContinue = true;
+        // Add summary ATX heading
         GitHubWorkflowHelper.WriteStepSummary("# InitialSetup\n\n");
         if (string.IsNullOrEmpty(actionEnvironment.ApiToken))
         {
+            // GitHub personal access token is missing
             Console.WriteLine("Entering actions secrets section");
             Announce($"Create an action secret {YouRataConstants.ApiTokenVariable} to store GitHub personal access token");
             GitHubWorkflowHelper.WriteStepSummary(
@@ -47,6 +53,7 @@ using (InitialSetupCommunicationClient client = new InitialSetupCommunicationCli
 
         if (canContinue && (!Regex.IsMatch(actionEnvironment.ApiToken, @"^ghp_[a-zA-Z0-9]{36}$")))
         {
+            // GitHub personal access token is invalid
             Console.WriteLine("Entering actions secrets verify section");
             Announce($"Action secret {YouRataConstants.ApiTokenVariable} is not a GitHub personal access token");
             canContinue = false;
@@ -55,9 +62,11 @@ using (InitialSetupCommunicationClient client = new InitialSetupCommunicationCli
         if (canContinue && ((string.IsNullOrEmpty(workflow.ProjectClientSecret) || string.IsNullOrEmpty(workflow.ProjectClientId)) ||
                             (workflow.ProjectClientSecret.Equals("empty") || workflow.ProjectClientId.Equals("empty"))))
         {
+            // YouTube Data API credentials are missing
             Console.WriteLine("Entering actions variables section");
             if (!config.ActionCutOuts.DisableUnsupportedGitHubAPI)
             {
+                // Create empty variables for the credentials
                 UnsupportedGitHubAPIClient.CreateVariable(actionEnvironment, YouRataConstants.ProjectClientIdVariable, "empty",
                     client.LogMessage);
                 client.Keepalive();
@@ -65,13 +74,16 @@ using (InitialSetupCommunicationClient client = new InitialSetupCommunicationCli
                     client.LogMessage);
                 client.Keepalive();
                 Announce("Fill repository variables and run action again");
+                // Add URL for action variables
                 GitHubWorkflowHelper.WriteStepSummary(
                     $"{actionEnvironment.EnvGitHubServerUrl}/{actionEnvironment.EnvGitHubRepository}/settings/variables/actions\n");
             }
             else
             {
+                // Tell the user to create empty variables for the credentials
                 Announce($"Create an action variable {YouRataConstants.ProjectClientIdVariable} to store Google API client ID");
                 Announce($"Create an action variable {YouRataConstants.ProjectClientSecretsVariable} to store Google API client secrets");
+                // Add URL for action variables
                 GitHubWorkflowHelper.WriteStepSummary(
                     $"{actionEnvironment.EnvGitHubServerUrl}/{actionEnvironment.EnvGitHubRepository}/settings/variables/actions\n");
             }
@@ -81,10 +93,13 @@ using (InitialSetupCommunicationClient client = new InitialSetupCommunicationCli
 
         if (canContinue && (string.IsNullOrEmpty(workflow.ProjectApiKey) || workflow.ProjectApiKey.Equals("empty")))
         {
+            // YouTube Data API key is missing
             Console.WriteLine("Entering Google API key section");
+            // Create an empty action secret for the key
             GitHubAPIClient.CreateOrUpdateSecret(actionEnvironment, YouRataConstants.ProjectApiKeyVariable, "empty", client.LogMessage);
             client.Keepalive();
             Announce($"Paste Google API key in action secret {YouRataConstants.ProjectApiKeyVariable}");
+            // Add URL for the action secret key
             GitHubWorkflowHelper.WriteStepSummary(
                 $"{actionEnvironment.EnvGitHubServerUrl}/{actionEnvironment.EnvGitHubRepository}/settings/secrets/actions/{YouRataConstants.ProjectApiKeyVariable}\n");
             canContinue = false;
@@ -92,21 +107,26 @@ using (InitialSetupCommunicationClient client = new InitialSetupCommunicationCli
 
         if (canContinue && (!YouTubeAuthHelper.GetTokenResponse(workflow.StoredTokenResponse, out _)))
         {
+            // TOKEN_RESPONSE is missing
             Console.WriteLine("Entering Google API stored token response section");
             GoogleAuthorizationCodeFlow flow = YouTubeAuthHelper.GetFlow(workflow.ProjectClientId, workflow.ProjectClientSecret);
             if (!string.IsNullOrEmpty(workflow.RedirectCode) && workflow.RedirectCode != "empty")
             {
+                // Authorization code is valid
                 string redirectTokenCode = workflow.RedirectCode.Trim().Replace("=", "").Replace("&", "");
                 AuthorizationCodeTokenRequest authorizationCodeTokenRequest =
                     YouTubeAuthHelper.GetTokenRequest(redirectTokenCode, workflow.ProjectClientId, workflow.ProjectClientSecret);
+                // Get a TokenResponse from the authorization code
                 TokenResponse? authorizationCodeTokenResponse =
                     YouTubeAuthHelper.ExchangeAuthorizationCode(authorizationCodeTokenRequest, flow);
                 client.Keepalive();
                 if (authorizationCodeTokenResponse != null)
                 {
+                    // TokenResponse is valid, save it to an action secret
                     YouTubeAuthHelper.SaveTokenResponse(authorizationCodeTokenResponse, actionEnvironment, client.LogMessage);
                     client.Keepalive();
                     Announce($"Google API stored token response has been saved to {YouRataConstants.StoredTokenResponseVariable}");
+                    // InitialSetup is complete, copy the errata contributing instructions to the repository root
                     workflow.CopyDirectionsReadme = true;
                 }
                 else
@@ -115,6 +135,7 @@ using (InitialSetupCommunicationClient client = new InitialSetupCommunicationCli
                     canContinue = false;
                 }
             }
+            // Create an empty action secret for the TokenResponse
             else if (GitHubAPIClient.CreateOrUpdateSecret(actionEnvironment, YouRataConstants.StoredTokenResponseVariable, "empty",
                          client.LogMessage))
             {
@@ -122,12 +143,14 @@ using (InitialSetupCommunicationClient client = new InitialSetupCommunicationCli
                 Uri url = flow.CreateAuthorizationCodeRequest(GoogleAuthConsts.LocalhostRedirectUri).Build();
                 GitHubAPIClient.CreateOrUpdateSecret(actionEnvironment, YouRataConstants.RedirectCodeVariable, "empty", client.LogMessage);
                 client.Keepalive();
+                // Tell the user to follow the link, allow the app, and paste the code from the URL
                 Announce("Follow this link to authorize this GitHub application");
                 Announce(url.ToString());
                 Announce("<ins>When finished it is normal that the site can't be reached</ins>");
                 Announce("Copy everything in the website URL between |code=| and |&|");
                 Announce($"Paste this value in action secret {YouRataConstants.RedirectCodeVariable}");
                 Announce("Run the action again within 30 seconds");
+                // Add URL for the action secret code
                 GitHubWorkflowHelper.WriteStepSummary(
                     $"{actionEnvironment.EnvGitHubServerUrl}/{actionEnvironment.EnvGitHubRepository}/settings/secrets/actions/{YouRataConstants.RedirectCodeVariable}\n");
                 canContinue = false;
@@ -141,6 +164,7 @@ using (InitialSetupCommunicationClient client = new InitialSetupCommunicationCli
 
         if (!canContinue)
         {
+            // Block all other milestones from startingx
             client.BlockAllMilestones();
         }
     }
@@ -153,6 +177,7 @@ using (InitialSetupCommunicationClient client = new InitialSetupCommunicationCli
     client.SetStatus(MilestoneCondition.MilestoneCompleted);
 }
 
+// Write to action step summary and console
 void Announce(string announcement)
 {
     Console.WriteLine(announcement);
